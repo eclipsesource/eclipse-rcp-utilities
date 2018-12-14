2ff46fedb738fb964ba299e28820db63423254d2
Stefan Dirix <sdirix@eclipsesource.com>
2018-12-14 12:04:18 +0100

    Initial commit

---
+++ src/org/eclipse/rcp/utilities/UISynchronizeImpl.java
@@ -0,0 +1,188 @@
+/*******************************************************************************
+ * Copyright (c) 2014 EM-SOFTWARE and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christoph Keimel <c.keimel@emsw.de> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.rcp.utilities;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.eclipse.fx.core.Subscription;
+import org.eclipse.fx.core.ThreadSynchronize;
+import org.eclipse.fx.core.log.Logger;
+import org.eclipse.fx.core.log.LoggerFactory;
+import org.eclipse.swt.widgets.Display;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * Implementation of the UISynchronize service for SWT.
+ * <p>
+ * Please note that this is a copy of org.eclipse.fx.ui.di.interopt.UISynchronizeImpl with a higher service ranking so
+ * that we use this implementation instead of the default JavaFX one which relies on Platform.runLater instead of
+ * Display.asyncExec.
+ * </p>
+ */
+@Component(service = { ThreadSynchronize.class }, property = { "service.ranking:Integer=2" })
+public class UISynchronizeImpl implements ThreadSynchronize {
+	private LoggerFactory factory;
+	private Logger logger;
+
+	/**
+	 * Setting a new factory
+	 *
+	 * @param factory
+	 *            the new factory
+	 */
+	@Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY)
+	public void setLoggerFactory(LoggerFactory factory) {
+		this.factory = factory;
+		this.logger = null;
+	}
+
+	/**
+	 * Unset the logger factory
+	 *
+	 * @param factory
+	 *            the factory to remove
+	 */
+	public void unsetLoggerFactory(LoggerFactory factory) {
+		if (this.factory == factory) {
+			this.factory = null;
+			this.logger = null;
+		}
+	}
+
+	private Logger getLogger() {
+		if (this.logger == null) {
+			this.logger = this.factory.createLogger(getClass().getName());
+		}
+		return this.logger;
+	}
+
+	@Override
+	public <V> V syncExec(final Callable<V> callable, V defaultValue) {
+		if (Display.getDefault().getThread() == Thread.currentThread()) {
+			try {
+				return callable.call();
+			} catch (Throwable t) {
+				throw new RuntimeException(t);
+			}
+		} else {
+			// We could also use Display.getDefault().syncExec(Runnable); but
+			// to keep semantics the same as in JavaFX we use the same code
+			// future task
+			RunnableFuture<V> task = new FutureTask<>(callable);
+			Display.getDefault().asyncExec(task);
+			try {
+				return task.get();
+			} catch (InterruptedException | ExecutionException e) {
+				getLogger().error("Unable to wait until the task is completed", e); //$NON-NLS-1$
+			} finally {
+				task.cancel(true);
+			}
+		}
+		return defaultValue;
+	}
+
+	@Override
+	public void syncExec(final Runnable runnable) {
+		if (Display.getDefault().getThread() == Thread.currentThread()) {
+			runnable.run();
+		} else {
+			// We could also use Display.getDefault().syncExec(Runnable); but
+			// to keep semantics the same as in JavaFX we use the same code
+			// future task
+			RunnableFuture<?> task = new FutureTask<Void>(runnable, null);
+			Display.getDefault().asyncExec(task);
+			try {
+				task.get(); // wait for task to complete
+			} catch (InterruptedException | ExecutionException e) {
+				getLogger().error("Unable to wait until the task is completed", e); //$NON-NLS-1$
+			} finally {
+				task.cancel(true);
+			}
+		}
+	}
+
+	@Override
+	public <V> Future<V> asyncExec(final Callable<V> callable) {
+		RunnableFuture<V> task = new FutureTask<>(callable);
+		Display.getDefault().asyncExec(task);
+		return task;
+	}
+
+	@Override
+	public void asyncExec(Runnable runnable) {
+		Display.getDefault().asyncExec(runnable);
+	}
+
+	@Override
+	public Subscription scheduleExecution(long delay, Runnable runnable) {
+		final AtomicBoolean b = new AtomicBoolean(true);
+		Runnable wrapper = () -> {
+			if (b.get()) {
+				runnable.run();
+			}
+		};
+		Display display = Display.getDefault();
+		display.asyncExec(() -> display.timerExec((int) delay, wrapper));
+
+		return new Subscription() {
+
+			@Override
+			public void dispose() {
+				b.set(false);
+				display.asyncExec(() -> display.timerExec(-1, runnable));
+			}
+		};
+	}
+
+	@Override
+	public <T> CompletableFuture<T> scheduleExecution(long delay, Callable<T> runnable) {
+		CompletableFuture<T> future = new CompletableFuture<>();
+
+		Display display = Display.getDefault();
+		display.asyncExec(() -> display.timerExec((int) delay, () -> {
+			try {
+				if (!future.isCancelled()) {
+					future.complete(runnable.call());
+				}
+			} catch (Throwable t) {
+				future.completeExceptionally(t);
+			}
+		}));
+
+		return future;
+	}
+
+	@Override
+	public boolean isCurrent() {
+		return Display.getDefault().getThread() == Thread.currentThread();
+	}
+
+	@Override
+	public <T> T block(BlockCondition<T> blockCondition) {
+		Display d = Display.getDefault();
+		while( blockCondition.isBlocked() ) {
+			if( ! d.readAndDispatch() ) {
+				d.sleep();
+			}
+		}
+		return null;
+	}
+
+}
